a0cd529653ead588657addd8acb99a5f849d29ee
aaa <aaa>
2024-04-15 14:17:09 +0500

    feat: create functions for students

--- app.js
+++ app.js
@@ -1,17 +1,79 @@
+const nameParent = document.querySelector('.nameParent');
+const parentNumber = document.querySelector('.parentNumber');
+const studentName = document.querySelector('.nameChild');
+const nameCourse = document.querySelector('.nameCourse');
+const formError = document.querySelector('.formError');
+const linkGit = document.querySelector('.linkGit')
+const notData = 'нет данных';
+
+function updateStudentParent(student) {
+
+  if (student.Agents && student.Agents.length > 0) {
+    let agent = student.Agents[0];
+    let firstName = agent.FirstName || '';
+    let middleName = agent.MiddleName || '';
+    let lastName = agent.LastName || '';
+    let mobileNumber = agent.Mobile || '';
+    let fullName = [firstName, middleName, lastName].filter(Boolean).join(' ');
+    nameParent.textContent = fullName.trim();
+    parentNumber.textContent = mobileNumber;
+  } else {
+    nameParent.textContent = notData;
+  }
+}
+
+function updateStudentName(student) {
+
+  if (student) {
+    let firstName = student.FirstName || '';
+    let lastName = student.LastName || '';
+    let fullName = [firstName, lastName].join(' ');
+    studentName.textContent = fullName.trim();
+  } else {
+    studentName.textContent = notData;
+  }
+}
+
+
+function updateStudentDisciplines(student) {
+
+  if (student) {
+    let discipline = student.Disciplines[0].Discipline || '';
+    let level = student.Disciplines[0].Level || '';
+
+    let description = [discipline, level].join(' ');
+
+    nameCourse.textContent = description;
+  } else {
+    nameCourse.textContent = notData;
+  }
+}
+
+function updateStudentLink(student) {
+
+  if (student) {
+    let link = student.ExtraFields[0].Value || '';
+
+    linkGit.textContent = ' ' + link;
+    linkGit.href = link;
+  } else {
+    linkGit.textContent = notData;
+  }
+}
+
+
 function handleResponse(data) {
-  // Обработка полученных данных
-  console.log('Полученные данные:', data);
 
-  // Очистка предыдущих данных
-  const employeeList = document.getElementById('employeeList');
-  employeeList.innerHTML = '';
+  console.log(data);
 
-  // Добавление каждого сотрудника в список
-  data.Employees.forEach(employee => {
-    const li = document.createElement('li');
-    li.textContent = `${employee.FirstName} ${employee.LastName} - ${employee.Phone ? employee.Phone : ''} ${employee.EMail}, ${employee.Offices.map(e => e.Name).join(' ')}`;
-    employeeList.appendChild(li);
-  });
+  if (data && data.Students && data.Students.length > 0) {
+    updateStudentParent(data.Students[0]);
+    updateStudentName(data.Students[0]);
+    updateStudentDisciplines(data.Students[0]);
+    updateStudentLink(data.Students[0])
+  } else {
+    formError.textContent = notData;
+  }
 }
 
 function fetchJSONP(url, callbackName) {
@@ -20,8 +82,27 @@ function fetchJSONP(url, callbackName) {
   document.body.appendChild(script);
 }
 
-const apiUrl = 'https://it-academy.t8s.ru/Api/V2/GetEmployees';
+const apiUrl = 'https://it-academy.t8s.ru/Api/V2/GetStudents';
 const authkey = 'WuLyujCFtTDaxq4MorahbYMnYBsWJa823uwSla62X4olBglySpRyXjTLmLvEMBfW';
 const callbackName = 'handleResponse';
 
-fetchJSONP(`${apiUrl}?authkey=${encodeURIComponent(authkey)}`, callbackName);
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+
+let studentId = null;
+
+input.addEventListener('change', (e) => {
+  e.preventDefault();
+  studentId = e.target.value;
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (parseInt(studentId) < 0) return;
+
+  formError.textContent = '';
+  nameParent.textContent = '';
+
+  fetchJSONP(`${apiUrl}?authkey=${encodeURIComponent(authkey)}&Id=${studentId}`, callbackName);
+})
