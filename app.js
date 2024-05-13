44afa2d3e39755eae30829d4a8dc9614e0beee5a
aaa <aaa>
2024-05-13 12:20:41 +0500

    create function for pdf file

--- app.js
+++ app.js
@@ -1,10 +1,70 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
 const nameParent = document.querySelector('.nameParent');
 const parentNumber = document.querySelector('.parentNumber');
 const studentName = document.querySelector('.nameChild');
 const nameCourse = document.querySelector('.nameCourse');
 const formError = document.querySelector('.formError');
-const linkGit = document.querySelector('.linkGit')
+const linkGit = document.querySelector('.linkGit');
+const nameGroup = document.querySelector('.nameGrorup');
+const nameTeacher = document.querySelector('.nameTeacher');
+const startEndDate = document.querySelector('.start-end-date');
+const dateVisit = document.querySelector('.dateVisit');
+const btnPdf = document.querySelector('.pdf__btn');
 const notData = 'нет данных';
+
+let studentId = null;
+let clientId = null;
+
+let beginTimeStudent = null;
+let endTimeStudent = null;
+
+let dateFrom = null;
+let dateTo = null;
+
+function updateBeginEndTime(student) {
+  if (student[0]?.BeginDate) {
+    beginTimeStudent = student[0].BeginDate;
+    endTimeStudent = student[0].EndDate;
+
+    startEndDate.textContent = `${beginTimeStudent}  -  ${endTimeStudent}`
+  } else {
+    startEndDate.textContent = notData;
+  }
+
+}
+
+function getDays(student) {
+  if (student[0]?.Days) {
+    console.log(student[0]?.Days);
+
+    let arr = student[0].Days
+
+    dateVisit.innerHTML = `
+    ${arr.map(e => {
+      return `<span>${e.Date} - ${e.Pass ? 'пропустил' : 'посетил'}</span>`
+    }).join('')}
+    
+    `
+  }
+
+}
+
+function checkDateRange() {
+  const currentDate = new Date();
+  const eightDaysAgo = new Date(currentDate.getTime() - 28 * 24 * 60 * 60 * 1000);
+
+  const currentDateStr = currentDate.toISOString().split('T')[0];
+  const eightDaysAgoStr = eightDaysAgo.toISOString().split('T')[0];
+
+
+  if (eightDaysAgoStr >= beginTimeStudent && currentDateStr <= endTimeStudent) {
+    dateFrom = eightDaysAgoStr;
+    dateTo = currentDateStr;
+
+  }
+}
+
 
 function updateStudentParent(student) {
 
@@ -17,8 +77,17 @@ function updateStudentParent(student) {
     let fullName = [firstName, middleName, lastName].filter(Boolean).join(' ');
     nameParent.textContent = fullName.trim();
     parentNumber.textContent = mobileNumber;
-  } else {
+  }
+  else {
     nameParent.textContent = notData;
+  }
+}
+
+function updateStudentId(student) {
+  if (student.ClientId) {
+    clientId = student.ClientId;
+  } else {
+    clientId = null;
   }
 }
 
@@ -34,16 +103,13 @@ function updateStudentName(student) {
   }
 }
 
-
 function updateStudentDisciplines(student) {
 
-  if (student) {
-    let discipline = student.Disciplines[0].Discipline || '';
-    let level = student.Disciplines[0].Level || '';
+  if (student && student[0]) {
 
-    let description = [discipline, level].join(' ');
+    let discipline = student[0].EdUnitDiscipline;
 
-    nameCourse.textContent = description;
+    nameCourse.textContent = discipline;
   } else {
     nameCourse.textContent = notData;
   }
@@ -51,7 +117,7 @@ function updateStudentDisciplines(student) {
 
 function updateStudentLink(student) {
 
-  if (student) {
+  if (student && student.ExtraFields) {
     let link = student.ExtraFields[0].Value || '';
 
     linkGit.textContent = ' ' + link;
@@ -62,18 +128,55 @@ function updateStudentLink(student) {
 }
 
 
+function updateGroupName(student) {
+
+  if (student && student[0]) {
+    let str = student[0].EdUnitName;
+    if (str) str = str.replace(/\s+/g, " ").split(' ');
+
+    nameGroup.textContent = str[0];
+    nameTeacher.textContent = str[1]?.replace(/\(|\)/g, " ");
+
+  } else {
+    nameGroup.textContent = notData;
+    nameTeacher.textContent = notData;
+  }
+}
+
 function handleResponse(data) {
 
-  console.log(data);
 
   if (data && data.Students && data.Students.length > 0) {
     updateStudentParent(data.Students[0]);
     updateStudentName(data.Students[0]);
-    updateStudentDisciplines(data.Students[0]);
-    updateStudentLink(data.Students[0])
+
+    updateStudentLink(data.Students[0]);
+    updateStudentId(data.Students[0]);
   } else {
     formError.textContent = notData;
   }
+}
+
+function handleResponse2(data) {
+  console.log(data);
+  if (data && data.EdUnitStudents && data.EdUnitStudents.length > 0) {
+    updateGroupName(data.EdUnitStudents)
+    updateStudentDisciplines(data.EdUnitStudents);
+    updateBeginEndTime(data.EdUnitStudents);
+    checkDateRange();
+    console.log(dateFrom, dateTo);
+  } else {
+    formError.textContent = notData;
+  }
+
+}
+
+function handleResponse3(data) {
+
+  console.log(data);
+  if (data && data.EdUnitStudents && data.EdUnitStudents.length > 0)
+    getDays(data.EdUnitStudents);
+
 }
 
 function fetchJSONP(url, callbackName) {
@@ -82,19 +185,21 @@ function fetchJSONP(url, callbackName) {
   document.body.appendChild(script);
 }
 
-const apiUrl = 'https://it-academy.t8s.ru/Api/V2/GetStudents';
-const authkey = 'WuLyujCFtTDaxq4MorahbYMnYBsWJa823uwSla62X4olBglySpRyXjTLmLvEMBfW';
-const callbackName = 'handleResponse';
-
-const form = document.querySelector('.form');
-const input = document.querySelector('.input');
-
-let studentId = null;
 
 input.addEventListener('change', (e) => {
   e.preventDefault();
   studentId = e.target.value;
+  e.target.value = '';
 })
+
+const apiUrl = 'https://it-academy.t8s.ru/Api/V2/GetStudents';
+const apiUrl2 = 'https://it-academy.t8s.ru/Api/V2/GetEdUnitStudents';
+
+const authkey = 'WuLyujCFtTDaxq4MorahbYMnYBsWJa823uwSla62X4olBglySpRyXjTLmLvEMBfW';
+const callbackName = 'handleResponse';
+const callbackName2 = 'handleResponse2';
+const callbackName3 = 'handleResponse3';
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -103,6 +208,55 @@ form.addEventListener('submit', (e) => {
 
   formError.textContent = '';
   nameParent.textContent = '';
+  nameParent.textContent = '';
+  parentNumber.textContent = '';
+  studentName.textContent = '';
+  nameCourse.textContent = '';
+  formError.textContent = '';
+  linkGit.textContent = '';
+  nameGroup.textContent = '';
 
   fetchJSONP(`${apiUrl}?authkey=${encodeURIComponent(authkey)}&Id=${studentId}`, callbackName);
+
+  setTimeout(() => {
+    fetchJSONP(`${apiUrl2}?authkey=${encodeURIComponent(authkey)}&studentClientId=${clientId}`, callbackName2);
+
+
+  }, 1000)
+
+  setTimeout(() => {
+    fetchJSONP(`${apiUrl2}?authkey=${encodeURIComponent(authkey)}&studentClientId=${clientId}&queryDays=true&dateFrom=${dateFrom}&dateTo=${dateTo}`, callbackName3);
+  }, 3000)
+
 })
+
+
+
+// Создание нового документа PDF
+
+function generatePdf() {
+
+  let docDefinition = {
+    content: [
+      `
+    ФИО Родителя:   ${nameParent.textContent}
+    Номер Родителя  ${parentNumber.textContent}
+
+    `
+    ]
+  };
+
+  pdfMake.createPdf(docDefinition).download('data.pdf');
+}
+
+
+btnPdf.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  generatePdf();
+
+})
+
+
+
+
